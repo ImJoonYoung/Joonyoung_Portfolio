cc63a0077231cf96e200f5cb20c0f1ff77a18c3b
junyoung <joonyoung319@naveer.com>
2023-03-25 17:43:56 +0900

    임준영 포트폴리오

--- js/common.js
+++ js/common.js
@@ -1,2 +1,14 @@
 /* 비쥬얼 메뉴 */
+$(document).ready(function() {
+   $('.visual_menu > li').on('mouseenter', function() {
+      $('.visual_menu > li').removeClass('close');
+      $('.visual_menu > li').eq($(this).index()).addClass('close');
+      $('.visual').css('width', '0px')
+   });
 
+   $('.visual_sec').on('mouseleave', function() {
+      $('.visual_menu > li').removeClass('close');
+   })
+
+
+});
