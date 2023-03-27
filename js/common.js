12e9ad7f8ab84104589250f21f5ca83be32a18a8
junyoung <joonyoung319@naveer.com>
2023-03-27 22:10:12 +0900

    임준영 포트폴리오

--- js/common.js
+++ js/common.js
@@ -10,5 +10,27 @@ $(document).ready(function() {
       $('.visual_menu > li').removeClass('close');
    })
 
+   let mainSlider = $('#pjSlide');
+   let innerText = $('.inner-text');
+
+   mainSlider.slick({
+      slidesToShow: 4,
+      dots: true,
+   });
+
+   mainSlider.on('wheel', function(e) {
+      e.preventDefault();
+
+      if (e.originalEvent.deltaY < 0) {
+         $(this).slick('slickPrev');
+      } else {
+         $(this).slick('slickNext');
+      }
+   });
+
+   innerText.on('wheel', function(e) {
+      e.stopPropagation();
+   })
+
 
 });
